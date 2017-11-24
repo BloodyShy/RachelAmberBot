70ce2ab895ff928992c1f9059eee4fa0cf58b8f0
BloodyShy <treysertreyser1488@gmail.com>
2017-11-25 05:08:07 +0800

    Update Rachel.js

--- Rachel.js
+++ Rachel.js
@@ -11,15 +11,5 @@ client.on('message', message => {
   	}
 });
 
-// Obtained using:
-voiceChannel.join()
-  .then(connection => {
-  get connection() {
-    const connection = this.guild.voiceConnection;
-    if (connection && connection.channel.id === this.id) return connection;
-    return null;
-  }
-  });
-
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.TOKEN);
